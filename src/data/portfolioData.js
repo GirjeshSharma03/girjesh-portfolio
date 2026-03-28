e943cb041e80c2fe224cd4505c94385d3f5424d3
girjeshsharma03 <grijesh6641@gmail.com>
2026-03-28 21:12:08 +0530

    link add project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -35,14 +35,14 @@ const portfolio = {
       description: "Designed a responsive furniture showcase website using HTML & CSS with clean UI and product layout.",
       tech: ["HTML5", "CSS3"],
       color: "#c8a96e",
-      link: "https://react-icons.github.io/react-icons/search/#q=link",
+      link: "https://girjeshsharma03.github.io/Furniture1/",
     },
     {
       name: "Street Shop Website",
       description: "Developed a street-fashion themed website featuring product sections and modern design.",
       tech: ["HTML5", "CSS3"],
       color: "#4ECDC4",
-      link: "https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_5szpgfto9i_e&adgrpid=155259813593&hvpone=&hvptwo=&hvadid=674893540034&hvpos=&hvnetw=g&hvrand=146661442455346112&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9062212&hvtargid=kwd-64107830&hydadcr=14452_2316413&gad_source=1",
+      link: "",
     },
     {
       name: "Brave Homepage Clone",
